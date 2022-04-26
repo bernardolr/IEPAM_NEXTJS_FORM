e702991c14be426bce407f832f5ff288c2ac849d
Bernardo Lopez <bernardolr17@gmail.com>
2022-04-26 14:45:06 -0500

    fixed error resolver yup

--- pages/form.js
+++ pages/form.js
@@ -1,34 +1,35 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers'
-import * as yup from 'yup'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export default function form() {
 
     const schema = yup.object({
         firstName: yup.string().required().max(80),
         lastName: yup.string().required().max(100),
-        Email: yup.string().required().max(80),
-        Message: yup.string().required().max(80),
+        Email: yup.string().required().email(),
+        Message: yup.string().required(),
     }).required()
 
-    const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(schema) });
+    const { register, handleSubmit, formState: {errors}} = useForm({resolver: yupResolver(schema),
+    mode:"onChange" });
 
     const onSubmit = data => console.log(data)
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} action='/' method='post'>
-            <input type='text' placeholder='First name' id='first' {...register("firstName", { required: true, maxLength: 80 })} />
+            <input type='text' placeholder='First name' id='first' {...register("firstName")} />
 
             {errors.firstName && 'First Name Required'}
 
-            <input type='text' placeholder='Last name' id='last' {...register("lastName", { required: true, maxLength: 100 })} />
+            <input type='text' placeholder='Last name' id='last' {...register("lastName")} />
 
             {errors.lastName && 'Last Name Required'}
 
-            <input type='email' placeholder='Email' id='email' {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} />
+            <input type='email' placeholder='Email' id='email' {...register("Email")} />
             {errors.Email && 'Email Required'}
-            <input type='textarea' placeholder='Message' id='message' {...register("Message", { required: true })} />
+            <input type='textarea' placeholder='Message' id='message' {...register("Message")} />
             {errors.Message && 'Message Required'}
             <button type='submit'>Submit</button>
 
