c67180beaa5efc7fa9c62cd5cf0f3310862dfb97
Bernardo Lopez <bernardolr17@gmail.com>
2022-04-26 16:25:38 -0500

    Added responsiveness and centered the card

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,15 @@ import Form from './form'
 
 export default function Home() {
   return (
-    <div class='card'>
-      <h5 class='card-header'>Formulario</h5>
+    <div class="container">
+      <div class="col-sm-12">
+        <div class='card' >
+          <h5 class='card-header'>Formulario</h5>
           <div class='card-body'>
             <Form />
           </div>
+        </div>
+      </div>
     </div>
     
   )
